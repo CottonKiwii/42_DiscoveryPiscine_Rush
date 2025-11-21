93de108cb11c6affba4afd471b49c296981c74d2
Julia Wolfram <julia.wolfram@gmx.at>
2025-11-21 15:12:52 +0100

    changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", () => {
-if (window.scrollY > 1000)
+if (window.scrollY > 800)
   document.getElementById("navigation").style.display = "block";
-if (window.scrollY < 1000)
+if (window.scrollY < 800)
   document.getElementById("navigation").style.display = "none";
 })
